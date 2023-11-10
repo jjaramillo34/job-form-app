3dca565f671475064e80da05605cf4dd4070c1be
Javier Jaramillo <jjaramillo34@gmail.com>
2023-11-10 15:12:24 -0500

    First Deploy for plan integral

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: {
+    crossOriginLoading: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/JotFormEmbed.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const JotFormEmbed = () => {
+  return (
+    <iframe
+      src="https://form.jotform.com/233130838141044"
+      title="JotForm Embed"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+        margin: "0",
+        padding: "0",
+      }}
+      allowFullScreen
+    ></iframe>
+  );
+};
+
+export default JotFormEmbed;
